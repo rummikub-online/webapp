db8f76651d5408e27fcf59a9937b5e67fdf508f6
arthaud-proust <arthaudp33@gmail.com>
2024-05-09 12:14:25 +0200

    feat: GameBoard turn system (begin, cancelModifications, points, end)

--- src/application/entities/GameBoard.test.ts
+++ src/application/entities/GameBoard.test.ts
@@ -2,18 +2,49 @@ import { CardCombination } from "./CardCombination";
 import { GameBoard } from "./GameBoard";
 
 describe("GameBoard", () => {
-  describe("backup", () => {
+  describe("turn", () => {
     test("can cancel modifications", () => {
       const gameBoard = new GameBoard({});
 
-      gameBoard.backup();
+      gameBoard.beginTurn();
       gameBoard.createCombination([{ color: "black", number: 7 }]);
-      gameBoard.restoreBackup();
+      gameBoard.cancelTurnModications();
 
       expect(gameBoard.toDto()).toStrictEqual({
         combinations: [],
         isValid: true,
       });
+    });
+
+    test("can compute turn points", () => {
+      const gameBoard = new GameBoard({});
+
+      gameBoard.beginTurn();
+      gameBoard.createCombination([
+        { color: "yellow", number: 10 },
+        { color: "black", number: 8 },
+      ]);
+
+      expect(gameBoard.turnPoints()).toBe(18);
+    });
+
+    test("cannot end turn if game board is not valid", () => {
+      const gameBoard = new GameBoard({});
+
+      gameBoard.beginTurn();
+      gameBoard.createCombination([
+        { color: "yellow", number: 10 },
+        { color: "black", number: 8 },
+      ]);
+
+      expect(() => gameBoard.endTurn()).toThrow(Error);
+    });
+
+    test("can end turn if game board is valid", () => {
+      const gameBoard = new GameBoard({});
+
+      gameBoard.beginTurn();
+      gameBoard.endTurn();
     });
   });
 
--- src/application/entities/GameBoard.ts
+++ src/application/entities/GameBoard.ts
@@ -6,8 +6,10 @@ import { CardCombination, ICardCombination } from "./CardCombination";
 export interface IGameBoard {
   createCombination(cards: CardListDto): void;
   isValid(): boolean;
-  backup(): void;
-  restoreBackup(): void;
+  beginTurn(): void;
+  cancelTurnModications(): void;
+  turnPoints(): number;
+  endTurn(): void;
   toDto(): GameBoardDto;
   points(): number;
 }
@@ -18,7 +20,7 @@ type GameBoardProps = {
 
 export class GameBoard implements IGameBoard {
   private combinations: Array<ICardCombination>;
-  private backupedData: GameBoardDto | null = null;
+  private previousTurn: GameBoardDto | null = null;
 
   constructor(props: GameBoardProps) {
     this.combinations = props.combinations ?? [];
@@ -36,21 +38,46 @@ export class GameBoard implements IGameBoard {
     return this.combinations.every((combination) => combination.isValid());
   }
 
-  backup(): void {
-    this.backupedData = this.toDto();
+  beginTurn(): void {
+    this.previousTurn = this.toDto();
   }
 
-  restoreBackup(): void {
-    if (!this.backupedData) {
-      throw new Error("No backup to restore");
-    }
+  cancelTurnModications(): void {
+    this.throwIfTurnHasNotStarted();
 
-    this.combinations = this.backupedData?.combinations.map(
+    this.combinations = this.previousTurn!.combinations.map(
       (combinationDto) =>
         new CardCombination({
           cards: combinationDto.cards,
         })
     );
+  }
+
+  turnPoints(): number {
+    this.throwIfTurnHasNotStarted();
+
+    const previousTurnPoints = cardCombinationsPoints(
+      this.previousTurn!.combinations
+    );
+
+    return this.points() - previousTurnPoints;
+  }
+
+  endTurn(): void {
+    this.throwIfTurnHasNotStarted();
+    this.throwIfNotValid();
+  }
+
+  private throwIfTurnHasNotStarted() {
+    if (!this.previousTurn) {
+      throw new Error("Turn has not started");
+    }
+  }
+
+  private throwIfNotValid() {
+    if (!this.isValid()) {
+      throw new Error("Game board is not valid");
+    }
   }
 
   toDto(): GameBoardDto {
