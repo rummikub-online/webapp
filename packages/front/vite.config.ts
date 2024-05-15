dd30aced275bd0c75d9490c1a0002c1074e2ef04
arthaud-proust <arthaudp33@gmail.com>
2024-05-15 12:42:51 +0200

    package: do not open vite & storybook on startup

--- packages/front/vite.config.ts
+++ packages/front/vite.config.ts
@@ -7,9 +7,6 @@ import svgLoader from "vite-svg-loader";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader()],
-  server: {
-    open: true,
-  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
