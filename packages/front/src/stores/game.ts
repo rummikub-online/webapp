c440a6e0afd03072d2a1e340c0e18b26a6097a28
arthaud-proust <arthaudp33@gmail.com>
2024-05-26 14:10:17 +0200

    feat: game store

---
+++ packages/front/src/stores/game.ts
@@ -0,0 +1,41 @@
+import { setupSocket } from "@/logic/socket";
+import type { GameBoardDto } from "@rumi/domain/dtos/gameBoard";
+import type { PlayerDto } from "@rumi/domain/dtos/player";
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useGameStore = defineStore("game", () => {
+  const player = ref<PlayerDto>();
+  const gameBoard = ref<GameBoardDto>();
+
+  const {
+    startGame,
+    cancelTurnModications,
+    drawCard,
+    endTurn,
+    moveCardAlone,
+    moveCardToCombination,
+    placeCardAlone,
+    placeCardInCombination,
+  } = setupSocket({
+    onPlayerUpdate(newPlayer) {
+      player.value = newPlayer;
+    },
+    onGameBoardUpdate(newGameBoard) {
+      gameBoard.value = newGameBoard;
+    },
+  });
+
+  return {
+    player,
+    gameBoard,
+    startGame,
+    cancelTurnModications,
+    drawCard,
+    endTurn,
+    moveCardAlone,
+    moveCardToCombination,
+    placeCardAlone,
+    placeCardInCombination,
+  };
+});
