a88cbb1ed8920a921837d5dc17f7e96b4a0749ca
arthaud-proust <arthaudp33@gmail.com>
2024-05-26 23:32:38 +0200

    feat: create combination drag zone

---
+++ packages/front/src/components/CreateCombinationDragZone.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type { ChangeEvent } from "@/lib/vueDraggable";
+import { toKey } from "@/logic/card";
+import { useGameStore } from "@/stores/game";
+import type { CardDto } from "@rumi/domain/dtos/card";
+import { ref, watch } from "vue";
+import draggable from "vuedraggable";
+import Card from "./Card.vue";
+
+const gameStore = useGameStore();
+
+const cards = ref([]);
+
+watch(
+  () => gameStore.player,
+  () => {
+    cards.value = [];
+  },
+);
+
+const handleChange = (e: ChangeEvent<CardDto>) => {
+  if (e.added) {
+    gameStore.cardDraggingHandler.to(null, null);
+  }
+};
+</script>
+<template>
+  <draggable
+    v-model="cards"
+    group="combinations"
+    tag="div"
+    class="justify-start items-start gap-0.5 inline-flex p-2 rounded-lg border-dashed border min-w-9 min-h-11 md:min-w-12 md:min-h-16"
+    :item-key="(card: CardDto) => toKey(card)"
+    @change="handleChange"
+  >
+    <template #item="{ element: card }">
+      <Card :color="card.color" :number="card.number" />
+    </template>
+  </draggable>
+</template>
