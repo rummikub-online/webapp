d256a1ba6b1ef9198c116cd856dda1f40a7a29b0
Arthaud <arthaudp33@gmail.com>
2024-09-30 19:39:15 +0200

    :lipstick: Center plus icon on create combination dragzone

--- components/CreateCombinationDragZone.vue
+++ components/CreateCombinationDragZone.vue
@@ -29,21 +29,22 @@ const handleChange = (e: ChangeEvent<CardDto>) => {
 };
 </script>
 <template>
-  <div class="p-2 px-4 relative text-card-create">
-    <PlusIcon
-      class="absolute size-4 md:size-6 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-    />
-
+  <div class="p-2 px-4">
     <Draggable
       v-model="cards"
       group="combinations"
       tag="div"
-      class="w-full h-full justify-start items-start gap-0.5 inline-flex rounded border-dashed border border-current min-w-9 min-h-11 md:min-w-12 md:min-h-16"
+      class="relative text-card-create justify-start items-start gap-0.5 inline-flex rounded border-dashed border border-current w-9 h-11 md:w-12 md:h-16"
       :item-key="(card: CardDto) => toKey(card)"
       @change="handleChange"
     >
       <template #item="{ element: card }">
         <Card :color="card.color" :number="card.number" />
+      </template>
+      <template #header>
+        <PlusIcon
+          class="absolute size-4 md:size-6 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
       </template>
     </Draggable>
   </div>
