5c02e22e484c313fb9530d7d273393a36cc363f4
MatthieuMarchand <matthieu.marchand@mmibordeaux.com>
2024-10-07 12:01:57 +0200

    :sparkles: button join game in home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,44 @@
 <template>
   <main class="h-screen bg-body-bg flex flex-col items-center justify-center">
-    <NuxtLink href="/games/create" target="_parent" no-prefetch class="mt-auto">
-      <Button type="primary">{{ t("pages.home.create_game") }}</Button>
-    </NuxtLink>
-    <label class="flex flex-col mt-auto mb-8">
-      <span>{{ t("pages.home.username") }}</span>
-      <input v-model="username" class="border p-1" />
-    </label>
+    <div class="flex flex-col items-center justify-center gap-8">
+      <NuxtLink
+        href="/games/create"
+        target="_parent"
+        no-prefetch
+        class="mt-auto"
+      >
+        <Button type="primary">{{ t("pages.home.create_game") }}</Button>
+      </NuxtLink>
+
+      <div class="flex gap-2">
+        <label class="flex flex-col mt-auto">
+          <span>{{ t("pages.home.code_join_game") }}</span>
+
+          <input
+            v-model="joinRoomId"
+            @keydown.enter="joinRoom"
+            class="border p-1"
+          />
+        </label>
+
+        <NuxtLink :href="getJoinRoomUrl" class="mt-auto">
+          <Button type="primary">{{ t("pages.home.join_game") }}</Button>
+        </NuxtLink>
+      </div>
+
+      <label class="flex flex-col mt-auto">
+        <span>{{ t("pages.home.username") }}</span>
+
+        <input v-model="username" class="border p-1" />
+      </label>
+    </div>
   </main>
 </template>
 <script setup lang="ts">
 const { username } = useUsername();
 const { t } = useI18n();
+
+const joinRoomId = ref("");
+const getJoinRoomUrl = computed(() => `/games/${joinRoomId.value}`);
+const joinRoom = () => navigateTo(getJoinRoomUrl.value);
 </script>
