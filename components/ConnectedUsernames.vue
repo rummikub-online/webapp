e79b970d538913f676200ff04aa280a0a5200d25
Arthaud <arthaudp33@gmail.com>
2024-10-19 13:16:48 +0200

    :lipstick: Pluralisation

--- components/ConnectedUsernames.vue
+++ components/ConnectedUsernames.vue
@@ -7,6 +7,9 @@ const props = defineProps<{
 }>();
 
 const { t } = useI18n();
+const connectedCount = computed(
+  () => Object.values(props.usernames).filter((v) => v).length,
+);
 
 const isOpen = ref(false);
 </script>
@@ -14,15 +17,15 @@ const isOpen = ref(false);
   <Button @click="isOpen = true">
     <div class="flex gap-2">
       <UserGroupIcon class="size-4 text-body-text" />
-      <span>{{
-        Object.values(usernames).filter((v) => v === true).length
-      }}</span>
+      <span>{{ connectedCount }}</span>
     </div>
   </Button>
   <UModal v-model="isOpen" :ui="{ background: 'bg-body-bg' }">
     <div class="p-4">
-      <h2 class="mb-4">{{ t("pages.game.connected_usernames") }}</h2>
-      <ul class="flex items-end gap-2 text-sm">
+      <h2 class="mb-4">
+        {{ t("pages.game.connected_usernames", connectedCount) }}
+      </h2>
+      <ul class="flex flex-col items-start gap-2 text-sm">
         <li v-for="(isConnected, username) in usernames" :key="username">
           <ConnectedUsername :username="username" :is-connected="isConnected" />
         </li>
